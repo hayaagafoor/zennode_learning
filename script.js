23ce2c64f9e869df828a54594720ace66c65c528
hayaagafoor <hayaaa0903@gmail.com>
2025-11-26 12:14:07 +0530

    Remove DS_Store and add .gitignore

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-// RANDOM WORDPRESS FACTS
-const facts = [
-  "WordPress was first released in 2003.",
-  "WordPress powers over 43% of all websites on the internet.",
-  "WordPress is written in PHP and uses MySQL.",
-  "WooCommerce is the most popular eCommerce plugin for WordPress.",
-  "There are over 60,000 free plugins in the WordPress directory.",
-  "WordPress themes control the design of your website."
-];
-//fact using api
-
+// Random wordpress facts using api
 const factBtn = document.getElementById("factBtn");
 const factText = document.getElementById("factText");
 
@@ -16,18 +6,18 @@ factBtn.addEventListener("click", () => {
   fetch('https://dummyjson.com/quotes/random')
     .then(res => res.json())
     .then(data => {
-      // data is a single random quote
+      // Data is a single random quote
       factText.textContent = `${data.quote}`;
     })
     .catch(err => console.error("Error fetching quote:", err));
 });
 
-//navbar in mobile
+//Navbar in mobile
 document.getElementById("menuToggle").addEventListener("click", function() {
   document.getElementById("navLinks").classList.toggle("active");
 });
 
-// SMOOTH SCROLL AND HIGHLIGHT CARD
+// Smooth scroll and highlighting card
 document.querySelectorAll('.section-index a').forEach(link => {
   link.addEventListener('click', function(e) {
     e.preventDefault();
